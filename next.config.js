ae62469aa371e852d9e883fdaf1c0f4d5b315f58
InnovixEdge <jay.cadmus@innovix-llc.com>
2025-06-20 11:11:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  // Remove the experimental outputFileTracingRoot that might be causing issues
   eslint: {
     ignoreDuringBuilds: true,
   },
