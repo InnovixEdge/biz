fe9b9e46e623b42a133ba4ba292f3c81a0409cc7
InnovixEdge <jay.cadmus@innovix-llc.com>
2025-06-20 12:21:59 -0400

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to Matallino AI Business Mentor</h1>
-      <p>Your AI-powered business guidance platform</p>
-    </div>
-  );
-}
